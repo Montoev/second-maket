d85de9b2eec45522ecd7892bd3615387365d8e1a
Montoev <148058918+Montoev@users.noreply.github.com>
2024-04-18 07:38:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+    function hoverOn() {
+    const spanElement = document.getElementById('hero__title-span');
+if (document.getElementById("hero__title-span")) {
+  spanElement.innerHTML = 'Cokteil';
+} 
+  }
+    function hoverOff() {
+    const spanElement = document.getElementById('hero__title-span');
+  spanElement.innerHTML = 'Cocteil';
+  }
+
