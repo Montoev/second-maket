ee84286ea860436074bba01e206c752fa4472825
Montoev <148058918+Montoev@users.noreply.github.com>
2024-05-14 21:57:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,3 +10,45 @@ if (document.getElementById("hero__title-span")) {
   spanElement.innerHTML = 'Cocteil';
   }
 
+  const swiperGallery = new Swiper('.gallery__slider', {
+    spaceBetween: 80,
+    slidesPerView: 4,
+  
+    navigation: {
+      nextEl: '.gallery__next',
+      prevEl: '.gallery__prev',
+    },
+  
+  });
+
+  const swiperTestimonials = new Swiper('.testimonials__slider', {
+    spaceBetween: 80,
+    slidesPerView: 2,
+  
+    navigation: {
+      nextEl: '.testimonials__next',
+      prevEl: '.testimonials__prev',
+    },
+  
+  });
+
+  const accordionLists = document.querySelectorAll('.accordion-list');
+  
+  accordionLists.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+      const accordionControl = e.target.closest('.accordion-list__control');
+      if (!accordionControl) return
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+
+      accordionItem.classList.toggle('.accordion-list__item--opened');
+
+      if (accordionItem.classList.contains('.accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null
+      }
+    })
+  })
